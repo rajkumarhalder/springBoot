dcf1166d8efe27b48a5a292994c395543859d79d
rajkumr.halder <rajkumr.halder@rajkumrhalder>
2018-04-05 23:22:33 +0530

    1 st commit

---
+++ myEntertainment/src/main/java/com/rajkumar/myEntertainment/MyEntertainmentApplication.java
@@ -0,0 +1,12 @@
+package com.rajkumar.myEntertainment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyEntertainmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyEntertainmentApplication.class, args);
+	}
+}
---
+++ myEntertainment/src/test/java/com/rajkumar/myEntertainment/MyEntertainmentApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rajkumar.myEntertainment;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyEntertainmentApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
